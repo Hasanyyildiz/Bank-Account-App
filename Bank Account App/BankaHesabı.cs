a24cb622fe5d04b9c06405f4c2ae056e587037e2
Hasan Yıldız <hy0782617@gmail.com>
2023-11-01 14:36:58 +0300

    Bank Account App
    Application that returns a small bank account in C# programming language
    
    (C# programlama dilinde küçük bir banka hesabı döndüren uygulama)

---
+++ Bank Account App/BankaHesabı.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankaHesabi
+{
+    internal class BankaHesabı
+    {
+        public string accountName = "undefined";
+        public string accountCredit = "undefined";
+ 
+    }
+
+    class VadesizHesap : BankaHesabı
+    {
+        public void IbanNo() 
+        {
+            Console.WriteLine("IBAN number: 4544 2417 2724 4894");
+        }
+
+    }
+
+    class TasarufHesabı:BankaHesabı
+    {
+        private double deposit;
+        private double interestRate;
+
+        public double Deposit
+        {
+            get
+            {
+                return deposit;
+            }
+            set
+            {
+                deposit = value;
+            }
+        }
+
+        public double InterestRate
+        {
+            get
+            {
+                return interestRate;
+            }
+            set
+            {
+                interestRate = value;
+            }
+        }
+        public double CalculateInterest()
+        {
+            return deposit = deposit * (1 + interestRate);
+        }
+
+
+
+    }
+}
---
+++ Bank Account App/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BankaHesabi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankaHesabı bankahesabi = new BankaHesabı();
+            Console.Write("Account Name: ");
+            bankahesabi.accountName = Console.ReadLine();
+
+            Console.Write("Credit Card : ");
+            bankahesabi.accountCredit = Console.ReadLine();
+
+            if (bankahesabi.accountCredit.Length > 8 || bankahesabi.accountCredit.Length != 8)
+            {
+                Console.WriteLine();
+                Console.WriteLine("User Not Found, Try Again...");
+                Console.WriteLine("(Kullanıcı Bulunamadı, Tekrar Deneyiniz)");
+                Console.WriteLine();
+                return;
+
+            }
+
+            VadesizHesap hesap = new VadesizHesap();
+            hesap.IbanNo();
+            Console.WriteLine("");
+            Console.WriteLine("Welcome To {0}", bankahesabi.accountName);
+            Console.WriteLine("");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("");
+            TasarufHesabı tasaruf = new TasarufHesabı();
+            Console.Write("Enter The Deposit: ");
+            tasaruf.Deposit = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the Interest Rate: ");
+            tasaruf.InterestRate = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Account Result: {0}", tasaruf.CalculateInterest());
+
+            Console.ReadKey();
+        }
+
+
+
+
+    }
+}
